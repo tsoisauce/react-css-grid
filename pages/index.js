786442bb5a91e4880d295b984ebbaf76a568b12c
Kelvin Tsoi <tsoikelvin@gmail.com>
2020-07-21 23:35:16 -0700

    refactor code for more dynamic grid

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,27 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>React CSS Grid</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <body>
-        <a href="/grid">CSS Grid</a>
-      </body>
-    </div>
+      <div className="container">
+        <nav>Navbar</nav>
+        <main>Main</main>
+        <div className="grid-items">
+          <div className="grid-item">Item 1</div>
+          <div className="grid-item">Item 2</div>
+          <div className="grid-item">Item 3</div>
+          <div className="grid-item">Item 4</div>
+          <div className="grid-item">Item 5</div>
+          <div className="grid-item">Item 6</div>
+          <div className="grid-item">Item 7</div>
+          <div className="grid-item">Item 8</div>
+        </div>
+        <footer>Footer</footer>
+      </div>
+    </>
   )
 }
